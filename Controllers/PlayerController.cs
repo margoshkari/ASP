512fb4456f838e7b6ae50a59f3d4dc191c3d958a
margo <student@25-03>
2022-02-07 10:19:47 +0200

    upd

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -9,7 +9,12 @@ namespace WebApplication7.Controllers
     [Route("[controller]")]
     public class PlayerController : ControllerBase
     {
-        private List<string> players = new List<string>() { "S1mple", "Electronic", "Boombl4", "Perfecto", "B1t", "B1ad3" };
+        private List<Player> players = new List<Player>() { new Player("S1mple", "Александр", "Костылев"),
+            new Player("Electronic", "Денис", "Шарипов"),
+             new Player("Boombl4", "Денис", "Шарипов"),
+              new Player("Perfecto", "Денис", "Шарипов"),
+               new Player("B1t", "Денис", "Шарипов"),
+                new Player("B1ad3", "Денис", "Шарипов") };
         private readonly ILogger<WeatherForecastController> _logger;
 
         public PlayerController(ILogger<WeatherForecastController> logger)
@@ -20,32 +25,32 @@ namespace WebApplication7.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return players;
+            //return players;
         }
 
         [HttpPost]
         public string Post(string item)
         {
-            players.Add(item);
+           // players.Add(item);
             return $"{item} added!";
         }
         [HttpDelete]
         public StatusCodeResult Delete(string item)
         {
-            if(players.FindIndex(x => x == item) == -1)
-            {
-                return StatusCode(204);
-            }
+           // if(players.FindIndex(x => x == item) == -1)
+           // {
+           //     return StatusCode(204);
+           // }
 
             return StatusCode(200);
         }
         [HttpPatch]
         public StatusCodeResult Patch(string _old, string _new)
         {
-            if (players.FindIndex(x => x == _old) != -1)
-            {
-                players[players.FindIndex(x => x == _old)] = _new;
-            }
+           // if (players.FindIndex(x => x == _old) != -1)
+          //  {
+           //     players[players.FindIndex(x => x == _old)] = _new;
+           // }
 
             return StatusCode(200);
         }
---
+++ Player.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication7
+{
+    public class Player
+    {
+        public string FirstName { get; set; }
+        public string SecondNme { get; set; }
+        public string Nickname { get; set; }
+        public Player(string nickname, string firstname, string secondname)
+        {
+            Nickname = nickname;
+            FirstName = firstname;
+            SecondNme = secondname;
+        }
+    }
+}
